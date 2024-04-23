fe5e9b9bd3e823d329ca99348956eb073873c0ea
partsguy73 <lellodrian@gmail.com>
2024-04-22 22:26:36 -0600

    Finished Prep1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("");
+
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+        Console.WriteLine();
     }
 }
