4629a9c459cd1e78f7d453ab6a3edd168589189a
partsguy73 <lellodrian@gmail.com>
2024-04-30 21:03:12 -0600

    Learning02 commit.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Store Manager";
+        job1._company = "O'Reilly Auto Parts";
+        job1._startYear = 2009;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Parts Specialist";
+        job2._company = "Stephen Wade Wholesale Parts";
+        job2._startYear = 2021;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Timothy Scott";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
