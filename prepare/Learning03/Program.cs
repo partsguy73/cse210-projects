9b3c6b04df0e1c458079d6c3a3bee5f6d0dfd599
partsguy73 <lellodrian@gmail.com>
2024-05-07 00:15:17 -0600

    Commit for Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Console.WriteLine("Hello Learning03 World!");
+        //Fraction with default values
+        Fraction f1 = new Fraction();
+        //print fraction
+        Console.WriteLine(f1.GetFractionString());
+        //print do the math
+        Console.WriteLine(f1.GetDecimalValue());
+
+        //fraction with 5
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        //fraction with 3,4
+        Fraction f3 = new Fraction(3,4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        //fraction with 1,3
+        Fraction f4 = new Fraction(1,3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
