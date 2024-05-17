1edb3223e85924f61c66963920f8fc60cc05a607
partsguy73 <lellodrian@gmail.com>
2024-05-16 23:45:57 -0600

    Update 5.16.2024

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        //Console.WriteLine("Hello Develop04 World!");
+        while(true)
+        {
+            Console.WriteLine("\nMenu Options:");
+            Console.WriteLine(" 1. Start breathing activity");
+            Console.WriteLine(" 2. Start reflecting activity");
+            Console.WriteLine(" 3. Start listing activity");
+            Console.WriteLine(" 4. Exit");  
+            Console.Write("Select a choice from the menu: ");
+            int menuChoice = int.Parse(Console.ReadLine());
+
+            switch (menuChoice)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a valid choice.");
+                    break;
+            }   
+        }
     }
 }
